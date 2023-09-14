5bb90a20e6bb3e27b28a12fa611263295ad45661
Vishal Kumar <vishal.ak5262@gmail.com>
2023-09-14 17:19:42 +0530

    Create patientController.js

---
+++ controllers/patientController.js
@@ -0,0 +1,38 @@
+const Patient = require('../models/patientModel');
+const Report = require('../models/reportModel');
+
+exports.register = async (req, res) => {
+  try {
+    const { phoneNumber } = req.body;
+    let patient = await Patient.findOne({ phoneNumber });
+    if (!patient) {
+      patient = new Patient({ phoneNumber });
+      await patient.save();
+    }
+    res.status(201).json({ message: 'Patient registered successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+};
+
+exports.createReport = async (req, res) => {
+  try {
+    const patientId = req.params.id;
+    const { doctorId, status } = req.body;
+    const report = new Report({ patient: patientId, doctor: doctorId, status });
+    await report.save();
+    res.status(201).json({ message: 'Report created successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+};
+
+exports.getAllReports = async (req, res) => {
+  try {
+    const patientId = req.params.id;
+    const reports = await Report.find({ patient: patientId }).sort({ date: 1 });
+    res.json(reports);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+};
