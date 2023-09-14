ee56d272f0e20bd6dc53ec857542bb9559c51fbb
Vishal Kumar <vishal.ak5262@gmail.com>
2023-09-14 19:15:07 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,25 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/login', authController.login);
+router.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      
+      // Call the appropriate function from authController to handle authentication
+      const isAuthenticated = await authController.authenticate(username, password);
+      
+      if (isAuthenticated) {
+        // Authentication succeeded, generate a token and send it in the response
+        const token = authController.generateToken(username); // Implement this function in authController
+        res.json({ token });
+      } else {
+        // Authentication failed
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 
 module.exports = router;
