a0c4d86470f39cddedf680283bfe81a8e1ca2f68
Vishal Kumar <vishal.ak5262@gmail.com>
2023-09-14 19:12:45 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-function generateJWTToken(doctorId) {
-  // Implementation of JWT token generation goes here
-  // You can use the 'jsonwebtoken' library for this purpose
-  const secretKey = 'your_secret_key'; // Replace this with your actual secret key
-  const token = jwt.sign({ doctorId }, secretKey, { expiresIn: '1h' });
-  return token;
-}
+exports.comparePasswords = async (inputPassword, hashedPassword) => {
+  try {
+    return await bcrypt.compare(inputPassword, hashedPassword);
+  } catch (error) {
+    throw error;
+  }
+};
