33b9c8c7b7adfbfa1d47e858bdc2a4e85300cd0a
Vishal Kumar <vishal.ak5262@gmail.com>
2023-10-03 00:21:46 +0530

    Update patientController.js

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -18,8 +18,8 @@ exports.register = async (req, res) => {
 exports.createReport = async (req, res) => {
   try {
     const patientId = req.params.id;
-    const { doctorId, status } = req.body;
-    const report = new Report({ patient: patientId, doctor: doctorId, status });
+    const { doctorName, status } = req.body;
+    const report = new Report({ patient: patientId, doctor: doctorName, status });
     await report.save();
     res.status(201).json({ message: 'Report created successfully.' });
   } catch (error) {
